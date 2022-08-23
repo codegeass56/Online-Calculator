cd85ebff7b64dd5e1d87ff489e431178839fd560
Siddharth Panjwani <siddharthkpanjwani@gmail.com>
2022-08-23 23:13:47 +0400

    Change buttons and implement 'add' operation on input
    Changed 'Undo' and 'Clear all' buttons to '.' and '=' buttons.
    Added functionality to perform add operation on user input.
    Changed and added global variables. All class names are now global
    variables.
    Added functionality to change display according to user state of
    calculation.
    
    Example:
    When adding 1 and 2:
    When 1 is selected, display shows 1.
    When plus is pressed and 2 is pressed, display is cleared and shows 2.
    When equals is pressed, display is cleared and result is shown.
    
    Note: Currently, only works for add operation on single digit integers.

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-function add(num1, num2) {
-  return num1 + num2;
+//Display screen
+const displayScreen = document.querySelector('.screen-content');
+
+//Button classes
+const numBtnClassName = 'number-button';
+const operatorBtnClassName = 'operator';
+const addBtnClassName = 'add-button';
+const subtractBtnClassName = 'subtract-button';
+const multiplyBtnClassName = 'multiply-button';
+const divideBtnClassName = 'divide-button';
+const equalsBtnClassName = 'equals-button';
+const dotBtnClassName = 'dot-button';
+
+let accumulator = 0;
+let currentOperator = null;
+
+function add(num1) {
+  accumulator += num1
 }
 
 function subtract(num1, num2) {
@@ -18,15 +34,27 @@ function divide(dividend, divisor) {
   return dividend / divisor;
 }
 
-function operate(num1, num2) {
-  return divide(num1, num2);
+function operate(operatorName, num1) {
+  switch (operatorName) {
+    case addBtnClassName: add(num1); break;
+    case subtractBtnClassName: subtract(num1, num2); break;
+    case multiplyBtnClassName: multiply(num1, num2); break;
+    case divideBtnClassName: divide(num1, num2); break;
+    case equalsBtnClassName:
+      if (currentOperator === 'add') {
+        add(num1);
+      }
+      displayScreen.textContent = accumulator;
+      accumulator = 0;
+      break;
+  }
 }
 
 //Get the grid container
 const buttonContainer = document.querySelector('.buttons-container');
 
 //Get the grid object
-const gridComputedStyle = window.getComputedStyle(buttonContainer);
+// const gridComputedStyle = window.getComputedStyle(buttonContainer);
 
 // // get number of grid rows
 // const gridRowCount = gridComputedStyle.getPropertyValue("grid-template-rows").split(" ").length;
@@ -34,30 +62,93 @@ const gridComputedStyle = window.getComputedStyle(buttonContainer);
 // const gridColumnCount = gridComputedStyle.getPropertyValue("grid-template-columns").split(" ").length;
 
 let number = 7;
+const rowSize = 4;
+const colSize = 4;
+
 //Create the buttons as grid items
-for (let rowCount = 0; rowCount < 4; rowCount++) {
-  for (let colCount = 0; colCount < 4; colCount++) {
+for (let rowCount = 0; rowCount < rowSize; rowCount++) {
+  for (let colCount = 0; colCount < colSize; colCount++) {
     const gridItem = document.createElement('button');
     if (colCount === 3) {
       switch (rowCount) {
-        case 0: gridItem.textContent = '+'; gridItem.classList.add('add-button'); number = 4; break;
-        case 1: gridItem.textContent = '-'; gridItem.classList.add('subtract-button'); number = 1; break;
-        case 2: gridItem.textContent = 'x'; gridItem.classList.add('multiply-button'); number = 0; break;
-        case 3: gridItem.textContent = '÷'; gridItem.classList.add('divide-button'); break;
+        case 0: gridItem.textContent = '+';
+          gridItem.classList.add(operatorBtnClassName);
+          gridItem.classList.add(addBtnClassName);
+          number = 4; break;
+
+        case 1: gridItem.textContent = '-';
+          gridItem.classList.add(operatorBtnClassName);
+          gridItem.classList.add(subtractBtnClassName);
+          number = 1; break;
+
+        case 2: gridItem.textContent = 'x';
+          gridItem.classList.add(operatorBtnClassName);
+          gridItem.classList.add(multiplyBtnClassName);
+          number = 0; break;
+
+        case 3: gridItem.textContent = '÷';
+          gridItem.classList.add(operatorBtnClassName);
+          gridItem.classList.add(divideBtnClassName);
+          break;
       }
     } else {
       if (rowCount === 3) {
         switch (colCount) {
-          case 0: gridItem.textContent = `${number}`; gridItem.classList.add('number-button'); break;
-          case 1: gridItem.textContent = 'Undo'; gridItem.classList.add('undo-button'); break;
-          case 2: gridItem.textContent = 'Clear All'; gridItem.classList.add('clear-button'); break;
+          case 0: gridItem.textContent = `${number}`;
+            gridItem.classList.add(numBtnClassName);
+            break;
+
+          case 1: gridItem.textContent = '.';
+            gridItem.classList.add(dotBtnClassName); break;
+
+          case 2: gridItem.textContent = '=';
+            gridItem.classList.add(operatorBtnClassName);
+            gridItem.classList.add(equalsBtnClassName);
+            break;
         }
       } else {
         gridItem.textContent = `${number}`;
-        gridItem.classList.add('number-button');
+        gridItem.classList.add(numBtnClassName);
         number++;
       }
     }
     buttonContainer.appendChild(gridItem);
   }
+}
+
+// Number buttons
+const numBtnArray = document.querySelectorAll('.number-button');
+
+for (let i = 0; i < numBtnArray.length; i++) {
+  numBtnArray[i].addEventListener('click', () => {
+    if (currentOperator === 'add' || currentOperator === 'subtract' ||
+      currentOperator === 'multiply' || currentOperator === 'divide') {
+      displayScreen.textContent = '';
+    }
+    displayScreen.textContent += numBtnArray[i].textContent;
+  });
+}
+
+
+//Operator buttons
+const operatorBtnArray = document.querySelectorAll('button.operator');
+
+for (let i = 0; i < operatorBtnArray.length; i++) {
+  operatorBtnArray[i].addEventListener('click', () => {
+    if (operatorBtnArray[i].classList.contains(addBtnClassName)) {
+      currentOperator = 'add';
+      operate(addBtnClassName, Number(displayScreen.textContent));
+    } else if (operatorBtnArray[i].classList.contains(subtractBtnClassName)) {
+      currentOperator = 'subtract';
+      operate(subtractBtnClassName, Number(displayScreen.textContent));
+    } else if (operatorBtnArray[i].classList.contains(multiplyBtnClassName)) {
+      currentOperator = 'multiply';
+      operate(multiplyBtnClassName, Number(displayScreen.textContent));
+    } else if (operatorBtnArray[i].classList.contains(divideBtnClassName)) {
+      currentOperator = 'divide';
+      operate(divideBtnClassName, Number(displayScreen.textContent));
+    } else if (operatorBtnArray[i].classList.contains(equalsBtnClassName)) {
+      operate(equalsBtnClassName, Number(displayScreen.textContent));
+    }
+  });
 }
